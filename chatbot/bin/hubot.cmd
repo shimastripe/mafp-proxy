6de8a52b61df6e276520c957af3f2ff893ec4534
Go Takagi <takagi@shimastripe.com>
2017-05-10 11:34:43 +0900

    Set config

--- chatbot/bin/hubot.cmd
+++ chatbot/bin/hubot.cmd
@@ -4,4 +4,12 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "poke" %* 
+SET HUBOT_SLACK_BOTNAME=
+SET HUBOT_SLACK_TEAM=
+SET HUBOT_SLACK_TOKEN=
+
+SET PORT=3000
+SET HUBOT_PROXY_MODE=chat
+SET PROXYCHATBOT_URL=http://localhost:2000
+
+node_modules\.bin\hubot.cmd %*
--- proxychatbot/bin/hubot.cmd
+++ proxychatbot/bin/hubot.cmd
@@ -4,4 +4,12 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "poke" %* 
+SET HUBOT_SLACK_BOTNAME=
+SET HUBOT_SLACK_TEAM=
+SET HUBOT_SLACK_TOKEN=
+
+SET PORT=2000
+SET HUBOT_PROXY_MODE=proxy
+SET CHATBOT_URL=http://localhost:3000
+
+node_modules\.bin\hubot.cmd %*
